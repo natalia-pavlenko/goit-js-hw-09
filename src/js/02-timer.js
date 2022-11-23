1954177d82046bdef60d57626decd6e2051710e8
natalia-pavlenko <npavl56@gmail.com>
2022-11-24 00:19:44 +0200

    stop null

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,53 @@
-import flatpickr from 'flatpickr';
 
+
+
+
+
+
+
+//     refs.btnStart.addEventListener('click', () => {
+//       const timerId = setInterval(() => {
+//         const deltaTime = selectedDates[0] - Date.now();
+//         if (deltaTime < 1000) {
+//           clearInterval(timerId);
+//         }
+//         const timeData = convertMs(deltaTime);
+//         const keys = Object.keys(timeData);
+//         for (const key of keys) {
+//           const timerRef = document.querySelector(`span[data-${key}]`);
+//           timerRef.textContent = timeData[key];
+//         }
+//       }, 1000);
+//       refs.btnStart.disabled = true;
+//     });
+//   },
+// }
+// flatpickr(refs.input, options);
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = addLeadingZero(Math.floor(ms / day));
+//   // Remaining hours
+//   const hours = addLeadingZero(Math.floor((ms % day) / hour));
+//   // Remaining minutes
+//   const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+//   // Remaining seconds
+//   const seconds = addLeadingZero(
+//     Math.floor((((ms % day) % hour) % minute) / second)
+//   );
+
+//   return { days, hours, minutes, seconds };
+// }
+// function addLeadingZero(value) {
+//   return String(value).padStart(2, '0');
+// }
+
+import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 const refs = {
@@ -10,7 +58,6 @@ const refs = {
   seconds: document.querySelector('span[data-seconds]'),
   input: document.querySelector('#datetime-picker'),
 };
-
 refs.btnStart.disabled = true;
 const options = {
   enableTime: true,
@@ -18,8 +65,11 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    if (selectedDates[0] - Date.now() < 0)
+    if (selectedDates[0] - Date.now() < 0) {
       Notiflix.Notify.failure('Please choose a date in the future');
+      return;
+    }
+    refs.btnStart.disabled = false;
     refs.btnStart.addEventListener('click', () => {
       const timerId = setInterval(() => {
         const deltaTime = selectedDates[0] - Date.now();
@@ -35,18 +85,15 @@ const options = {
       }, 1000);
       refs.btnStart.disabled = true;
     });
-    refs.btnStart.disabled = false;
   },
 };
 flatpickr(refs.input, options);
-
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
   const days = addLeadingZero(Math.floor(ms / day));
   // Remaining hours
@@ -57,7 +104,6 @@ function convertMs(ms) {
   const seconds = addLeadingZero(
     Math.floor((((ms % day) % hour) % minute) / second)
   );
-
   return { days, hours, minutes, seconds };
 }
 function addLeadingZero(value) {
